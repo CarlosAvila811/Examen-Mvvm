3d6d5e8771cd482dda105b6157472822f80e28f0
CarlosAvila811 <ca03899@gmail.com>
2024-09-17 19:15:53 -0600

    examen 1

--- Examen-Mvvm/App.xaml.cs
+++ Examen-Mvvm/App.xaml.cs
@@ -1,12 +1,10 @@
-﻿namespace Examen_Mvvm
-{
+﻿using Examen_Mvvm;
+
     public partial class App : Application
     {
         public App()
         {
-            InitializeComponent();
-
-            MainPage = new AppShell();
+        InitializeComponent();
+            MainPage = new MainPage();
         }
     }
-}
---
+++ Examen-Mvvm/viewModels/MainViewModel.cs
@@ -0,0 +1,54 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+
+namespace Examne_Mvvm.ViewModels
+{
+    public partial class MainViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private double producto1;
+
+        [ObservableProperty]
+        private double producto2;
+
+        [ObservableProperty]
+        private double producto3;
+
+        [ObservableProperty]
+        private double subtotal;
+
+        [ObservableProperty]
+        private double descuento;
+
+        [ObservableProperty]
+        private double totalPagar;
+
+        [RelayCommand]
+        public void Calcular()
+        {
+            // Calcular subtotal
+            Subtotal = Producto1 + Producto2 + Producto3;
+
+            // Determinar descuento
+            if (Subtotal >= 10000)
+                Descuento = Subtotal * 0.30;
+            else if (Subtotal >= 5000)
+                Descuento = Subtotal * 0.20;
+            else if (Subtotal >= 1000)
+                Descuento = Subtotal * 0.10;
+            else
+                Descuento = 0;
+
+            // Calcular total a pagar
+            TotalPagar = Subtotal - Descuento;
+        }
+
+        [RelayCommand]
+        public void Limpiar()
+        {
+            Producto1 = Producto2 = Producto3 = 0;
+            Subtotal = Descuento = TotalPagar = 0;
+        }
+    }
+}
